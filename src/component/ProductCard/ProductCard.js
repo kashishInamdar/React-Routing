ead09c9775ed8e5255a0085f453344b30ebfd43f
kashishInamdar <kashishinamdar2004@gmail.com>
2023-09-18 17:28:27 +0530

    Creat Increement dicreement Product Card

---
+++ src/component/ProductCard/ProductCard.js
@@ -0,0 +1,33 @@
+import './ProductCard.css'
+import Button from '../Button/Button'
+import { useState } from 'react'
+
+function Counter() {
+    const [count, setCount] = useState(0); 
+  
+    return (
+      <>
+      <div className='Increment-decrement'>
+        <button onClick={() => setCount(count - 1)}>-</button>
+        <p>{count}</p>
+        <button onClick={() => setCount(count + 1)}>+</button>
+        </div>
+      </>
+    );
+  }
+
+
+export default function ProductCard({heading , ImgUrl}){
+    return(
+    <>
+    <div className='ProductCard'>
+        <img src={ImgUrl} alt=''/>
+        <div>
+            <h1>{heading}</h1>
+            <Counter />
+           <center><Button text={'Add to Card'} /></center>
+        </div>
+    </div>
+    </>
+    )
+}
--- src/viwes/Home/Home.js
+++ src/viwes/Home/Home.js
@@ -1,10 +1,19 @@
 import Navbar from '../../component/Navbar/Navbar'
 import Button from '../../component/Button/Button'
+import ProductCard from '../../component/ProductCard/ProductCard'
+import './Home.css'
+
+import pizza1 from './pizza1.jpeg'
 export default function Home(){
     return(
         <>
         <Navbar/>
         <h1 className='heading'>This is the Home Page</h1>
+        <div className='productCard'>
+        <ProductCard ImgUrl={pizza1} heading={'Mioniz Pizza'}/> 
+        <ProductCard ImgUrl={pizza1} heading={'Mioniz Pizza'}/> 
+        <ProductCard ImgUrl={pizza1} heading={'Mioniz Pizza'}/> 
+        </div>
         <br/>
         <Button text="Home Page"/>
         </>
